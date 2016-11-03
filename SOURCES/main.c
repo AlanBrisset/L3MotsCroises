6a8f817cb36ad75265b568a863ec4f8d1a700e72
Youssef Khatiri <youssef.khatiri@etudiant.univ-lr.fr>
2016-11-03 17:55:43 +0100

    Tentative de synchronisaton avec GIT

--- SOURCES/main.c
+++ SOURCES/main.c
@@ -6,12 +6,12 @@
  */
 
 #include <stdio.h>
-#include <zmq.h>
+//#include <zmq.h>
 #include <string.h>
 #include <unistd.h>
 
 int main() {
 
-        printf ("Hello Leo!\n"); // Si HELLO WORLD apparait dans la console, Votre config est OK !
+        printf ("Hello Youssef!\n"); // Si HELLO WORLD apparait dans la console, Votre config est OK !
         return 0;
 }
